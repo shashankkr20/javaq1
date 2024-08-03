6ea48d5dc0cdae1392dfba3751f4bd573c3d70e4
Shashank Kumar <111291676+shashankkr20@users.noreply.github.com>
2024-08-03 06:58:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Set the target time to today 6:30 AM IST
     const targetISTTime = new Date(currentISTDate);
-    targetISTTime.setHours(6, 58, 0, 0);
+    targetISTTime.setHours(10, 0, 0, 0);
 
     if (currentISTDate >= targetISTTime) {
         showPDF();
